4c47ab7bb7bcd8ce3d647a8865195f276dd2a6dc
itsLyLee <lyylee00@gmail.com>
2020-10-06 16:00:38 +0200

    update

--- routes/articles.js
+++ routes/articles.js
@@ -1,10 +1,15 @@
 const express = require("express");
+const Article = require(./../models/article)
 const router = express.Router();
 
 router.get("/new", (req, res) => {
   res.render("articles/new");
 });
 
-router.post("/", (req, res) => {});
-
+router.post("/", (req, res) => {
+    const article = new Article({
+        
+    })
+});
+    
 module.exports = router;
